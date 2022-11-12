e7402c56ea4cbf34c5c8d78d335ea3974e4e1f63
Richard Grin <grin@unice.fr>
2022-11-12 11:15:55 +0100

    Etat initial.

---
+++ src/main/java/fr/grin/tpcustomer/resources/JakartaEE91Resource.java
@@ -0,0 +1,20 @@
+package fr.grin.tpcustomer.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee9")
+public class JakartaEE91Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
