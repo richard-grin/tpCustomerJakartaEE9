d82ec9ec3a7844a94d0388a9f988c9c58f008ac9
Richard Grin <grin@unice.fr>
2022-11-13 08:40:11 +0100

    Ajout EJB CustomerManager.

---
+++ src/main/java/fr/grin/tpcustomer/ejb/CustomerManager.java
@@ -0,0 +1,31 @@
+package fr.grin.tpcustomer.ejb;
+
+import fr.grin.tpcustomer.entities.Customer;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+
+/**
+ * Gère la persistance des Customers.
+ */
+@Stateless
+public class CustomerManager {
+
+    @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+
+    public void persist(Customer customer) {
+        em.persist(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        Query query = em.createNamedQuery("Customer.findAll");
+        return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+        return em.merge(customer);
+    }
+}
